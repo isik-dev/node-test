9125cd2511382827369f69d337276f4623b1c99e
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-01 21:47:57 +0900

    added few things

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer(function (req, res) {
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), function (err, content) {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' });
+    //         res.end(content);
+    //     })
+    // }
+    // else if (req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), function (err, content) {
+    //         if (err) throw err;
+    //         res.writeHead(200, { 'Content-Type': 'text/html' });
+    //         res.end(content);
+    //     })
+    // }
+    // else if (req.url === '/api/users') {
+    //     const users = [
+    //         {name: 'James Boiler', age: 45},
+    //         {name: 'Alexa', age: 29001}
+    //     ]
+    //     res.writeHead(200, { 'Content-Type': 'application/JSON' })
+    //     res.end(JSON.stringify(users))
+    // }
 
-logger.on('message', function (data) {
-    console.log('Called listener', data);
+    // Build file path
+    
+
 })
 
-logger.log('Hello World');
+const PORT = process.env.PORT || 5000
 
+server.listen(PORT, function () {console.log(`Server running on PORT: ${PORT}`);})
--- logger.js
+++ logger.js
@@ -8,4 +8,16 @@ class Logger extends EventEmitter {
     }
 }
 
-module.exports = Logger;
+// module.exports = Logger;
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', function (data) {
+    console.log('Called listener', data);
+})
+
+logger.log('Hello World');
+
+ 
